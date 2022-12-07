50c4e9d513fff610fc4f07a80146c874134028e7
Dani Esquilas <desquilas@gmail.com>
2022-12-07 12:01:17 +0100

    Refactored solution

--- day07/main.py
+++ day07/main.py
@@ -1,24 +1,10 @@
 import pytest
 
-def calculate_directory_size(path, directories, files_in_directories, directories_bigger = [], free_space = False):
-    size = 0
-    for file in files_in_directories[path]:
-        size += int(file[1])
-    for dir in directories[path]:
-        size += calculate_directory_size(path + "/" + dir, directories, files_in_directories, directories_bigger)
-
-    if free_space and size > free_space:
-        directories_bigger.append(size)
-
-    return size
-
-
 def day_7(filename):
 
     lines = [l.strip() for l in open(filename).readlines()]
 
-    directories = {}
-    files_in_directories = {}
+    directories_size = {}
     path = []
 
     for i in range(len(lines)):
@@ -28,35 +14,30 @@ def day_7(filename):
         elif "$ cd " in current_line:
             path.append(current_line.split()[-1])
         if current_line == "$ ls":
-            current_directory = lines[i-1].split(" ")[-1]
-            directories["/".join(path)] = []
-            files_in_directories["/".join(path)] = []
+            directories_size["/".join(path)] = 0
             i += 1
             while i < len(lines) and not "$ cd" in lines[i]:
-                if "dir" in lines[i]:
-                    dir = lines[i].split(" ")[-1]
-                    directories["/".join(path)].append(dir)
-                else:
-                    file_size, file_name = lines[i].split(" ")
-                    files_in_directories["/".join(path)].append((file_name, file_size))
+                if not "dir" in lines[i]:
+                    file_size= lines[i].split(" ")[0]
+                    for j in range (1, len(path) + 1):
+                        directories_size["/".join(path[:j])] += int(file_size)
                 i += 1
 
     output_p1 = 0
 
-    root_size = calculate_directory_size("/", directories, files_in_directories)
-    total_size = 70000000
-    needed_size = 30000000
-    free_space_needed = needed_size - (total_size - root_size)
+    free_space = 30000000 - (70000000 - directories_size["/"])
+    bigger_directories = []
 
-    directories_bigger = []
+    for directory in directories_size:
+        if directories_size[directory] <= 100000:
+            output_p1 += directories_size[directory]
+        if free_space < directories_size[directory]:
+            bigger_directories.append(directories_size[directory])
 
-    for directory in directories:
-        current_size = calculate_directory_size(directory, directories, files_in_directories, directories_bigger, free_space_needed)
-        if current_size <= 100000:
-            output_p1 += current_size
+    bigger_directories.sort()
 
-    directories_bigger.sort()
-    output_p2 = directories_bigger[0]
+    output_p2 = bigger_directories[0]
+
 
     return output_p1, output_p2
 
@@ -66,7 +47,6 @@ def test_day_7():
 
 
 test_day_7()
-
 
 p1, p2 = day_7("input.txt")
 
